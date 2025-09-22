9c8edf772521791987f4e78e430f1f1d7912f6da
Sebastian Moller <74718321+jsinc-sebastian@users.noreply.github.com>
2025-09-22 18:45:06 +0200

    Update index.php

--- webroot/index.php
+++ webroot/index.php
@@ -102,7 +102,7 @@ $phpVersion = phpversion();
     <div class="message">Webserver correctly installed and running!</div>
     <div class="webroot-message">You can now place your website files in the webroot folder</div>
     <div class="footer">
-        <p>Powered by <a href="https://sigmaprods.dev" target="_blank">sigmaprods.dev</a></p>
+        <p>Powered by <a href="https://clutchvps.no" target="_blank">clutchvps.no</a></p>
     </div>
 </div>
 
